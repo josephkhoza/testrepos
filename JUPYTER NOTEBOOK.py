a1dd090dfb89af99a82f2c0a9b346232c76b2984
JOSEPH KHOZA <144587850+josephkhoza@users.noreply.github.com>
2023-09-10 11:17:24 +0200

    Add files via upload

---
+++ JUPYTER NOTEBOOK.py
@@ -0,0 +1,161 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Notebook
+
+# ## Introduction ##
+
+# In[ ]:
+
+
+# Data Science Language
+
+
+# 1. Python
+# 2. R
+# 3. SQL
+# 4. Scala
+# 5. Julia
+# 6. Java
+# 7. C++
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#Data science libraries
+
+
+# 1. Numpy
+# 2. Pandas
+# 3. Seaborn
+# 4. metplotlib
+# 5. scikitlearn
+# 6. ggplot
+# 7. caret
+# 8. stringr
+# 9. dplyr
+
+# In[ ]:
+
+
+#Table of data science tool
+
+
+# |Language|Library|
+# |-----|-----|
+# |Python|Numpy|
+# |Python|Pandas|
+# |R|scielearn|
+
+# In[ ]:
+
+
+# Arethmetic expression
+ 
++ First expression
++ Second expression
++ Third expression
++ Fourth expression
+
+- First expression
+- Second expression
+- Third expression
+- Fourth expression
+
+* First expression
+* Second expression
+* Third expression
+* Fourth expression
+
+1. First expression
+2. Second expression
+3. Third expression
+4. Fourth expression
+
+
+# In[ ]:
+
+
+
+
+
+# In[1]:
+
+
+# multiplaction and addition cell
+2*2+2
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# convert minutes to hours
+
+
+# In[37]:
+
+
+time = float(input(" time in minutes: "))
+
+
+# In[43]:
+
+
+hours = time // 60
+
+
+# In[44]:
+
+
+hours
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# List of objectives
+
+
+# 1. Understand the basics of SQL
+# 2. Understand the basics of Python
+# 3. Understand the language to use depending on the problem to solve
+# 4. Able to tell a story from a data through utilisation of visualisation tools
+# 5. Become proficient in data analytics and use of computational tools
+
+# In[ ]:
+
+
+# Author
+
+
+# Joseph Khoza
+
+# In[ ]:
+
+
+# Github
+
+
+# In[ ]:
+
+
+
+
